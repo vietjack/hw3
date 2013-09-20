e37b3759ed557f5be320f283569052236869c11a
Zackary Johnson <zackary.w.johnson@gmail.com>
2013-09-20 16:49:05 -0400

    initial commit

---
+++ src/com/javangon/matchinggame/GameActivity.java
@@ -0,0 +1,14 @@
+package com.javangon.matchinggame;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class GameActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_game);
+	}
+}
